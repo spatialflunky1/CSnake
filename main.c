0d4079c36c961a99efe8e7a000df7b336a03f408
Spatialflunky1 <jfandrus84@gmail.com>
2023-05-17 06:55:55 -0400

    upload main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Windows.h>
+
+WindowProc();
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE pInstance, PWSTR cmd, int showCmd) {
+    //initialize the application
+    const wchar_t CLASS_NAME[] = L"";
+    WNDCLASS wc = {};
+
+    //create the window
+    //loop the program
+}
